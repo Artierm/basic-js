c7c64c2082676fc964f3c992f95a8503780ccac3
Artierm <artyom.rus.ct100@mail.ru>
2020-02-27 23:04:11 +0300

    init:carbon-dating&hanoi-tower

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,38 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(arrayData) {
+ 
+  if ( arrayData === undefined ||  typeof arrayData !="string")
+
+  {
+      return false;
+  }
+  if( arrayData.length==0)
+  {
+    return false; 
+  }
+  else
+  {
+   
+    arrayData = parseFloat(arrayData);
+
+    if (arrayData <= 0)
+    {
+      return false;
+    }
+
+   if (typeof arrayData == "number"  && arrayData<=MODERN_ACTIVITY )
+
+    {
+    return Math.ceil((Math.log(MODERN_ACTIVITY/arrayData))/((0.693)/HALF_LIFE_PERIOD));
+    }
+
+    else
+     {
+        return false;
+    }
+    
+    
+  }
 };
