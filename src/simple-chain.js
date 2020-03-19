808568ad20d88c939a434c91ea03d814510c664b
Artierm <artyom.rus.ct100@mail.ru>
2020-03-19 23:34:06 +0300

    do it

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,35 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
-};
+  array : [],
+    getLength() {
+      if (this.array.length==0)  {
+        return 0;
+      }
+    return this.array.length;
+    },
+    addLink(value) {
+    this.array.push(value);
+    return this;
+    },
+    removeLink(position) {
+        if( !Number.isInteger(position) || position < 1 ||  position > this.array.length ){
+          this.array =[];
+          throw Error;
+        }
+        this.array.splice(position-1,1);
+        return this;
+    },
+    reverseChain() {
+      this.array.reverse();
+      return this;
+    },
+    finishChain() {
+     //return this.array.join('');
+     let result = this.array.map(item =>`( ${item === undefined ? '' : item} )`).join('~~');
+     this.array =[];
+     return result;
+    }
+  };
+  module.exports = chainMaker;
 
-module.exports = chainMaker;
+// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain(), '( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )')
+  
